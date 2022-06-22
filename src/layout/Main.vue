657ad4e07348ee7b4bc190b3a88b3b706cca0081
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-06-21 22:23:22 -0300

    fix: test mobile

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -5,30 +5,26 @@
   <div class="bg-white w-full h-[60px] border-b-2 z-50">
     <div class="flex justify-between mx-10">
       <div class="flex pt-4 text-indigo-500">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-7 w-7"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-          />
+        <div class="mr-3 mt-1 md:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:bg-gray-200 hover:rounded-xl hover:p-1" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd" />
+          </svg>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+          stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
         </svg>
 
         <h1 class="font-extrabold">Sparkle</h1>
       </div>
 
       <div class="mt-3">
-        <img
-          class="w-8 h-8 rounded-full shrink-0"
-          src="https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=900&h=600&fit=crop"
-          alt="avatar"
-        />
+        <img class="w-8 h-8 rounded-full shrink-0"
+          src="https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=900&h=600&fit=crop" alt="avatar" />
       </div>
     </div>
   </div>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,7 @@ import Menu from "../layout/Menu.vue";
 import TitlePage from "../components/TitlePage.vue";
 import Card from "../components/Card.vue";
 
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
 const hours = computed(() => {
   const now = new Date();
@@ -12,34 +12,43 @@ const hours = computed(() => {
     "Hoje é " + now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear()
   );
 });
+const size = ref<boolean>(true);
 
 const ativades = ref<string>("Reunião as 19:00");
 
 const setor = ref<string>("Administrativo");
+
+onMounted(() => {
+  if (window.screen.width <= 756) {
+    size.value = false
+  } else { !size.value  }
+})
+
 </script>
 
 <template>
   <Main />
   <div class="flex">
-    <Menu />
+    <Menu v-show="size" />
     <div class="m-6">
       <TitlePage title="Home" subtitle="Seja bem vindo a o Sparkle!" />
 
       <div class="flex flex-col gap-4 md:flex-wrap md:flex-row">
+        <Card color="blue" title="Atividades para hoje:" grafic="grafic" />
+
+        <Card color="blue" title="Ganho Semanal:" grafic="grafic" />
+
+        <Card color="blue" title="Atividades semana passada:" grafic="grafic" />
+      </div>
+
+      <div class="flex flex-col gap-4 md:flex-wrap md:flex-row mt-4">
         <Card color="blue" title="Organize seu dia:" :subtitle="hours" />
 
         <Card color="blue" title="Setor de atuação:" :subtitle="setor" />
 
         <Card color="blue" title="Número de atividades semanais:" :subtitle="ativades" />
       </div>
-
-      <div class="mt-4">
-        <Card
-          color="blue"
-          title="Atividades para hoje:"
-          grafic="grafic"
-        />
-      </div>
     </div>
   </div>
 </template>
+
