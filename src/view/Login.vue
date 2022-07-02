3be76979fa02eed4ccb5ca0de8c4f33d04182581
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-07-02 18:03:51 -0300

    refactor: ajust login

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+const email = ref<string>("")
+const password = ref<string>("")
+
+const router = useRouter();
 
 function shipmmentLogin() {
-  alert("hey")
+  console.log(email +' '+ password)
+  router.push({ name: 'Home' })
 }
 </script>
 
 <template>
-  <div class="flex justify-between flex-row items-center m-10">
+  <div class="w-full">
 
-    <form @submit="shipmmentLogin" class="flex flex-col">
-      <label>E-mail</label>
-      <input type="email" name="email" id="email">
+    <div class="flex flex-row items-center justify-between">
 
-      <label>Password</label>
-      <input type="email" name="email" id="email">
+      <form @submit="shipmmentLogin" class="flex flex-col w-[350px]">
+        <label>E-mail</label>
+        <input type="email" name="email" id="email" class="px-2 py-1 mb-4 focus:border-indigo-500"
+          placeholder="josesantos@gmail.com" v-model="email">
 
-      <input type="submit" value="Login" />
+        <label>Password</label>
+        <input type="password" name="password" id="password" class="px-2 py-1 mb-4 focus:border-indigo-500"
+          placeholder="*********" v-model="password">
 
-      <button :disabled="true">Ainda não tenho conta</button>
-    </form>
+        <input type="submit" value="Login" class="px-2 py-1 mb-4 text-white bg-indigo-700" />
 
-    <img src="../assets/Login-talk.svg" alt="login-img" class="w-[450px]" />
+        <router-link to="/" class="text-center text-blue-700 hover:text-blue-200">Ainda não tenho conta?</router-link>
+      </form>
+
+      <div>
+        <img src="../assets/Login-talk.svg" alt="login-img" class="w-[450px] h-[450px]" />
+      </div>
+
+    </div>
 
   </div>
 </template>
