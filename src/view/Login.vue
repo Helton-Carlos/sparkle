477a22226152a7b935a1e422d9c195389d89cc63
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-07-02 01:25:54 -0300

    feature:page login

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+function shipmmentLogin() {
+  alert("hey")
+}
 </script>
 
 <template>
-  <div class="flex">
-    <div>Login</div>
+  <div class="flex justify-between flex-row items-center m-10">
+
+    <form @submit="shipmmentLogin" class="flex flex-col">
+      <label>E-mail</label>
+      <input type="email" name="email" id="email">
+
+      <label>Password</label>
+      <input type="email" name="email" id="email">
+
+      <input type="submit" value="Login" />
+
+      <button :disabled="true">Ainda não tenho conta</button>
+    </form>
+
+    <img src="../assets/Login-talk.svg" alt="login-img" class="w-[450px]" />
+
   </div>
 </template>
