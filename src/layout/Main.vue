088c9077b28c64adc6c3115ea51f7ce03087cf3c
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-07-02 01:06:24 -0300

    fix: route user

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
-function onMenu():void{
+const showTabs = ref<boolean>(false)
+
+function onMenu(): void {
   alert("hey")
 }
 </script>
@@ -10,7 +13,8 @@ function onMenu():void{
     <div class="flex justify-between mx-7 md:mx-10">
       <div class="flex pt-4 text-indigo-500">
         <div class="mt-1 mr-3 md:hidden" @click="onMenu">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-gray-200 hover:rounded-xl hover:p-1" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-gray-200 hover:rounded-xl hover:p-1"
+            viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
               clip-rule="evenodd" />
@@ -27,8 +31,22 @@ function onMenu():void{
       </div>
 
       <div class="mt-3">
-        <img class="w-8 h-8 rounded-full shrink-0"
-          src="https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=900&h=600&fit=crop" alt="avatar" />
+        <img class="w-8 h-8 rounded-full shrink-0 cursor-pointer"
+          src="https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=900&h=600&fit=crop" alt="avatar"
+          @click="showTabs = !showTabs" />
+
+        <div class="ml-3 relative" v-show="showTabs">
+          <div
+            class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+
+            <router-link to="/login" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"
+              tabindex="-1" id="user-menu-item-0">Your Profile</router-link>
+
+            <router-link to="/login" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"
+              tabindex="-1" id="user-menu-item-2">Sign out</router-link>
+          </div>
+        </div>
       </div>
     </div>
   </div>
