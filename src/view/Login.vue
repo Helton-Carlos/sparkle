aa1b411b266adfb4edadf5ee37d8d95c098c93af
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-07-03 19:58:27 -0300

    feature: config part one

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,41 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRouter } from 'vue-router'
 
 const email = ref<string>("")
 const password = ref<string>("")
 
-const router = useRouter();
-
 function shipmmentLogin() {
-  console.log(email +' '+ password)
-  router.push({ name: 'Home' })
+  console.log(email + ' ' + password)
+ // router.push({ name: 'Home' })
 }
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="flex items-center justify-between mx-32 mt-20">
 
-    <div class="flex flex-row items-center justify-between">
-
-      <form @submit="shipmmentLogin" class="flex flex-col w-[350px]">
-        <label>E-mail</label>
-        <input type="email" name="email" id="email" class="px-2 py-1 mb-4 focus:border-indigo-500"
-          placeholder="josesantos@gmail.com" v-model="email">
-
-        <label>Password</label>
-        <input type="password" name="password" id="password" class="px-2 py-1 mb-4 focus:border-indigo-500"
-          placeholder="*********" v-model="password">
-
-        <input type="submit" value="Login" class="px-2 py-1 mb-4 text-white bg-indigo-700" />
-
-        <router-link to="/" class="text-center text-blue-700 hover:text-blue-200">Ainda não tenho conta?</router-link>
-      </form>
-
+    <form @submit="shipmmentLogin()" class="flex flex-col w-[400px]">
       <div>
-        <img src="../assets/Login-talk.svg" alt="login-img" class="w-[450px] h-[450px]" />
+        <h1 class="mb-4 text-3xl font-semibold">Login</h1>
       </div>
+      
+      <label>E-mail:</label>
+      <input type="email" name="email" id="email" class="px-2 py-2 mt-1 mb-4 rounded-md focus:border-indigo-50"
+        placeholder="josesantos@gmail.com" :value="email">
 
+      <label>Password:</label>
+      <input type="password" name="password" id="password" class="px-2 py-2 mt-1 mb-4 rounded-md focus:border-indigo-500"
+        placeholder="*********" :value="password">
+
+      <input type="submit" value="Login" class="px-2 py-2 mb-4 text-white bg-indigo-700 rounded-md shadow hover:bg-indigo-900" />
+
+      <router-link to="/" class="text-center text-blue-700 hover:text-blue-200">Ainda não tenho conta?</router-link>
+    </form>
+
+    <div>
+      <img src="../assets/Login-talk.svg" alt="login-img" class="w-[450px] h-[450px]" />
     </div>
 
   </div>
--- src/view/Settings.vue
+++ src/view/Settings.vue
@@ -2,6 +2,11 @@
 import Main from "../layout/Main.vue";
 import Menu from "../layout/Menu.vue";
 import TitlePage from "../components/TitlePage.vue";
+import func from "../../vue-temp/vue-editor-bridge";
+
+function change(){
+  alert("hey")
+}
 </script>
 
 <template>
@@ -9,10 +14,76 @@ import TitlePage from "../components/TitlePage.vue";
   <div class="flex">
     <Menu />
     <div class="m-6">
-      <TitlePage
-        title="Configurações"
-        subtitle="Deixe seu sistema com seu estilo."
-      />
-    </div>
+      <TitlePage title="Configurações" subtitle="Deixe seu sistema com seu estilo." />
+
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+          <div class="mt-5 md:mt-0 md:col-span-2">
+            <form @submit="change">
+              <div class="overflow-hidden shadow sm:rounded-md">
+                <div class="px-4 py-5 space-y-6 bg-white sm:p-6">
+                  <fieldset>
+                    <legend class="sr-only">By Email</legend>
+                    <div class="text-base font-medium text-gray-900" aria-hidden="true">Configurações de tela </div>
+                    <div class="mt-4 space-y-4">
+                      <div class="flex items-start">
+                        <div class="flex items-center h-5">
+                          <input id="comments" name="comments" type="checkbox"
+                            class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500">
+                        </div>
+
+                        <div class="ml-3 text-sm">
+                          <label for="comments" class="font-medium text-gray-700">Comentários</label>
+                          <p class="text-gray-500">Liberar comentários na tela inicial.</p>
+                        </div>
+                      </div>
+
+                      <div class="flex items-start">
+                        <div class="flex items-center h-5">
+                          <input id="candidates" name="candidates" type="checkbox"
+                            class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500">
+                        </div>
+
+                        <div class="ml-3 text-sm">
+                          <label for="candidates" class="font-medium text-gray-700">QR-CODE</label>
+                          <p class="text-gray-500">Autorizar qr-code em tela inicial.</p>
+                        </div>
+                      </div>
+
+                    </div>
+                  </fieldset>
+                  <fieldset>
+                    <legend class="text-base font-medium text-gray-900 contents">Setor de atuação</legend>
+                    <p class="text-sm text-gray-500">Escolha como será configurado suas informações.</p>
+                    <div class="mt-4 space-y-4">
+                      <div class="flex items-center">
+                        <input id="push-everything" name="push-notifications" type="radio"
+                          class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500">
+                        <label for="push-everything" class="block ml-3 text-sm font-medium text-gray-700">Administrativo
+                        </label>
+                      </div>
+                      <div class="flex items-center">
+                        <input id="push-email" name="push-notifications" type="radio"
+                          class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500">
+                        <label for="push-email" class="block ml-3 text-sm font-medium text-gray-700"> Desenvolivmento
+                        </label>
+                      </div>
+                      <div class="flex items-center">
+                        <input id="push-nothing" name="push-notifications" type="radio"
+                          class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500">
+                        <label for="push-nothing" class="block ml-3 text-sm font-medium text-gray-700">Suporte
+                        </label>
+                      </div>
+                    </div>
+                  </fieldset>
+                </div>
+                <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
+                  <button type="submit"
+                    class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Salvar</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
